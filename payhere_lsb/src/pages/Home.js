22909f6fa10591ea8ff6368b3b6c98a267204ca1
[sungbumv] <sungbumv@gmail.com>
2022-04-23 20:31:30 +0900

    feat : 토큰 변경

--- payhere_lsb/src/pages/Home.js
+++ payhere_lsb/src/pages/Home.js
@@ -33,7 +33,7 @@ function MainFunction() {
       axios
         .get(apiUrl + inputData, {
           headers: {
-            Authorization: "token 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", // github api 초과로 인한 access token 발급
+            Authorization: "token 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", // github api 초과로 인한 access token 발급
           },
         })
         .then(function (response) {
